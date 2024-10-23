23c5ed7eb05ec8f5827f93401fcdb1bf74e177e1
Adonat74 <donatgoninet.antoine@gmail.com>
2024-10-23 20:58:49 +0200

    made a board component and a cell component

---
+++ src/components/Board.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import mazeList from "../resources/mazes.json"
+import { onMounted, ref } from 'vue'
+
+const props = defineProps({
+  size: Number,
+  version: Number
+})
+
+let maze = ref([]);
+
+function loadMaze() {
+    maze.value = mazeList[props.size][`ex-${props.version}`];
+}
+
+
+
+onMounted(() => loadMaze());
+
+</script>
+
+<template>
+    <div v-for="cell in maze">
+        <div>{{ cell.posX }}</div>
+    </div>
+  
+</template>
+
+<style scoped>
+
+</style>
