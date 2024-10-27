71a5ee507aac2f105f170f21960e7cdd91c70896
Adonat74 <donatgoninet.antoine@gmail.com>
2024-10-27 18:16:13 +0100

    made a first intuition algorythm

--- src/solvers/mazeSolver.js
+++ src/solvers/mazeSolver.js
@@ -5,22 +5,31 @@ function delay(ms) {
 
 export async function solveMaze(maze) {
     const exitCell = maze.find(cell => cell.getExit === true);
+    let currentCell;
 
 
     setStartCell();
+    currentCell = getCurrent();
 
 
 
+    while(currentCell && !exitCell.getCurrentCell) {
 
-    while(!exitCell.getCurrentCell) {
-
-
+        console.log('turn');
         await delay(1000);
+
+        if (!currentCell.getWalls[0]) {
+            changeCurrentCell(0);
+        } else if (!currentCell.getWalls[1]) {
+            changeCurrentCell(1);
+        } else if (!currentCell.getWalls[2]) {
+            changeCurrentCell(2);
+        } else if (!currentCell.getWalls[3]) {
+            changeCurrentCell(3);
+        }
+        currentCell = getCurrent();
     }
 
-    console.log(exitCell)
-    console.log(maze.find(cell => cell.getEntrance))
-    console.log(getCurrentCell())
 
 
 
@@ -32,16 +41,39 @@ export async function solveMaze(maze) {
     //
     // }
 
-    function setStartCell() {
-        maze.find(cell => cell.getEntrance).setCurrentCell = true;
-        maze.find(cell => cell.getEntrance).setVisited = true;
+    function changeCurrentCell(dir) {
+        let oldCurrent = getCurrent();
+
+        if (dir === 0) {
+            currentCell = maze.find(cell => cell.getY === oldCurrent.getY-1);
+        } else if (dir === 1) {
+            currentCell = maze.find(cell => cell.getX === oldCurrent.getX+1);
+        } else if (dir === 2) {
+            currentCell = maze.find(cell => cell.getY === oldCurrent.getY+1);
+        } else if (dir === 3) {
+            currentCell = maze.find(cell => cell.getX === oldCurrent.getX-1);
+        }
+
+        currentCell.setVisited = true;
+        oldCurrent.setCurrentCell = false;
+        currentCell.setCurrentCell = true;
     }
 
-    function getCurrentCell() {
+
+
+
+
+
+    function getCurrent() {
         return maze.find(cell => cell.getCurrentCell === true);
     }
 
 
+    function setStartCell() {
+        const startCell = maze.find(cell => cell.getEntrance);
+        startCell.setCurrentCell = true;
+        startCell.setVisited = true;
+    }
 
 
 }
